4d3cc5d8df6a224fa8aaf4bbcd03362594aada68
bragceo <119948453+bragceo@users.noreply.github.com>
2023-05-17 17:50:23 -0600

    fix bug in app.js

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const dotenv = require('dotenv');
 const db = require('./config/database');
 
 // Test DB
-db.authenticate()
-    .then(() => console.log('Database connected...'))
-    .catch(err => console.log('Error: ' + err))
+// db.authenticate()
+//     .then(() => console.log('Database connected...'))
+//     .catch(err => console.log('Error: ' + err))
 
 const app = express();
 
