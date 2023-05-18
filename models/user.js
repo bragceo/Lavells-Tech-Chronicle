3358df8d4dcd8fccf9ba20cce0a7b99e6b6c2dee
bragceo <119948453+bragceo@users.noreply.github.com>
2023-05-17 18:10:40 -0600

    revise models to fix bug

--- models/user.js
+++ models/user.js
@@ -1,14 +1,41 @@
-const Sequelize = require('sequelize');
-const db = require('../config/database');
+// const Sequelize = require('sequelize');
+// const db = require('../config/database');
 
-const User = db.define('user', {
+// const User = db.define('user', {
+//     username: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//         unique: true
+//     },
+//     email: {
+//         type: Sequelize.STRING,
+//         validate: {
+//             isEmail: true
+//         },
+//         allowNull: false,
+//         unique: true
+//     },
+//     password: {
+//         type: Sequelize.STRING,
+//         allowNull: false
+//     }
+// });
+
+// module.exports = User;
+
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/database');
+
+class User extends Model {}
+
+User.init({
     username: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
         unique: true
     },
     email: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
         validate: {
             isEmail: true
         },
@@ -16,9 +43,12 @@ const User = db.define('user', {
         unique: true
     },
     password: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
         allowNull: false
     }
+}, {
+    sequelize,
+    modelName: 'User'
 });
 
 module.exports = User;
